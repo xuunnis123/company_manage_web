16f66d594eca954103de97457d8c69650eb7c544
Ji-Yang Lin <xuunnis123@gmail.com>
2021-11-30 23:06:39 +0800

    remove extra files and add Order Scren

---
+++ frontend/src/screens/OrderCreateScreen.js
@@ -0,0 +1,209 @@
+import React, {useState,useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector} from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card, Dropdown,DropdownButton } from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import {  listSupplier } from '../actions/supplierActions'
+import {  addProduct } from '../actions/productActions'
+function OrderCreateScreen({ match, location, history}) {
+    /*name = models.CharField(max_length=200, null=True, blank=True)
+    model = models.CharField(max_length=200, null=True, blank=True)
+    unit = models.CharField(max_length=200, null=True, blank=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    cost = models.IntegerField(null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    modifiedAt = models.DateTimeField(auto_now = True) */
+    const [supplierTitle,setSupplierTitle] = useState('請選擇供應商')
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [cost, setCost] = useState(0)
+    const [category, setCategory] = useState('')
+    const [countInStock, setCountInStock] = useState(0)
+    const [model, setModel] = useState('')
+    const [unit, setUnit] = useState('')
+    const [supplier, setSupplier] = useState('')
+    const [memo, setMemo] = useState('')
+
+    const dispatch = useDispatch()
+  
+    //const redirect = location.search ? location.search.split('=')[1] :'/school'
+    const redirect = '/product'
+
+    const supplierList = useSelector(state => state.supplierList)
+    const { supplier_error, supplier_loading, suppliers } = supplierList
+
+    const productAdd = useSelector(state => state.productAdd)
+    const {error, loading, product} = productAdd
+
+    useEffect(()=>{
+        dispatch(listSupplier())
+        if(product){
+            
+            history.push(redirect)
+        }
+        
+    },[history, product, redirect])
+    const handleSelect=(e)=>{
+        
+        var splitSupplier = e.split(',');
+        
+        setSupplier(splitSupplier[0])
+
+        setSupplierTitle(splitSupplier[1]);  
+      }
+
+    //dispatch(listSchool())
+    const submitHandler =(e) =>{
+        e.preventDefault()
+        
+        dispatch(addProduct(name, price, cost, supplier, model, category, countInStock, unit, memo))
+        
+        history.push(redirect)
+        
+    }
+    return (
+        <FormContainer>
+            <h1>新增品項</h1>
+            
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            
+            <Form onSubmit={submitHandler}>
+
+            <Form.Group controlId='name'>
+                                <Form.Label>品項名稱</Form.Label>
+                                <Form.Control
+
+                                    type='name'
+                                    placeholder='輸入品項'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId='price'>
+                                <Form.Label>售價</Form.Label>
+                                <Form.Control
+
+                                    type='number'
+                                    placeholder='輸入售價'
+                                    value={price}
+                                    onChange={(e) => setPrice(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+
+                            <Form.Group controlId='cost'>
+                                <Form.Label>成本</Form.Label>
+                                <Form.Control
+
+                                    type='text'
+                                    placeholder='輸入成本價'
+                                    value={cost}
+                                    onChange={(e) => setCost(e.target.value)}
+                                >
+                                </Form.Control>
+
+                            </Form.Group>
+
+                            <Form.Group controlId='supplier'>
+                                <Form.Label>供應商</Form.Label>
+                                <DropdownButton
+                                aligndown="true"
+                                title= {supplierTitle}
+                                id="dropdown-menu-align-down"
+                                onSelect={handleSelect}
+                                    >
+
+                            {suppliers.map((supplier,index) =>{
+                            
+                            return <Dropdown.Item eventKey={[supplier._id,supplier.name]} key={index}>{supplier.name}</Dropdown.Item>
+                            })}
+                                    
+                            </DropdownButton>
+                            
+                            </Form.Group>
+
+                            <Form.Group controlId='category'>
+                                <Form.Label>類型</Form.Label>
+                                <Form.Control
+
+                                    type='text'
+                                    placeholder='輸入類型'
+                                    value={category}
+                                    onChange={(e) => setCategory(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId='countinstock'>
+                                <Form.Label>庫存</Form.Label>
+                                <Form.Control
+
+                                    type='number'
+                                    placeholder='輸入庫存'
+                                    value={countInStock}
+                                    onChange={(e) => setCountInStock(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId='model'>
+                                <Form.Label>型號</Form.Label>
+                                <Form.Control
+
+                                    type='text'
+                                    placeholder='輸入型號'
+                                    value={model}
+                                    onChange={(e) => setModel(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId='unit'>
+                                <Form.Label>單位</Form.Label>
+                                <Form.Control
+
+                                    type='text'
+                                    placeholder='輸入單位'
+                                    value={unit}
+                                    onChange={(e) => setUnit(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId='memo'>
+                                <Form.Label>備註</Form.Label>
+                                <Form.Control
+
+                                    type='text'
+                                    placeholder='輸入備註'
+                                    value={memo}
+                                    onChange={(e) => setMemo(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                <Button type='submit' variant='primary'>
+                    建立
+                </Button>
+            </Form>
+        
+            <Row className='py-3'>
+                <Col>
+                     <Link to='/product'>
+                     取消
+                        </Link>
+                </Col>
+            </Row>
+        </FormContainer>
+    )
+}
+export default OrderCreateScreen
---
+++ frontend/src/screens/OrderListScreen.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Paginate from '../components/Paginate'
+import { listProducts, deleteProduct, addProduct } from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
+
+function OrderListScreen({ history, match }) {
+
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
+    const productDelete = useSelector(state => state.productDelete)
+    const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
+
+   
+
+    let keyword = history.location.search
+    useEffect(() => {
+        dispatch({ type: PRODUCT_CREATE_RESET })
+        console.log("dd")
+        dispatch(listProducts())
+        console.log("ee")
+        /*
+        if (successCreate) {
+            history.push(`/product/${createdProduct._id}/edit`)
+        } else {
+            dispatch(listProducts(keyword))
+            
+        }
+        */
+
+    }, [dispatch, history, successDelete, keyword])
+
+
+    const deleteHandler = (id) => {
+
+        if (window.confirm('是否確定要刪除品項？')) {
+            dispatch(deleteProduct(id))
+        }
+    }
+
+    const createProductHandler = () => {
+        history.push('/product/create')
+    }
+
+    return (
+        <div>
+            <Row className='align-items-center'>
+                <Col>
+                    <h1>品項</h1>
+                </Col>
+
+                
+            </Row>
+            <Col className='text-right'>
+                    <Button className='my-3' onClick={createProductHandler}>
+                        <i className='fas fa-plus'></i> 新增品項
+                    </Button>
+            </Col>
+            {loadingDelete && <Loader />}
+            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+
+
+         
+
+            {loading
+                ? (<Loader />)
+                : error
+                    ? (<Message variant='danger'>{error}</Message>)
+                    : (
+                        <div>
+                            <Table striped bordered hover responsive className='table-sm'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>商品名稱</th>
+                                        <th>價格</th>
+                                        <th>成本價</th>
+                                        <th>分類</th>
+                                        <th>數量</th>
+                                        <th>單位</th>
+                                        <th>經銷商</th>
+                                        <th>備註</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    
+
+                                    {products.map(oneProduct => (
+                                        <tr key={oneProduct._id}>
+                                            <td>{oneProduct._id}</td>
+                                            <td>{oneProduct.name}</td>
+                                            <td>{oneProduct.price}</td>
+                                            <td>{oneProduct.cost}</td>
+                                            <td>{oneProduct.category}</td>
+                                            <td>{oneProduct.countInStock}</td>
+                                            <td>{oneProduct.unit}</td>
+                                            <td>{oneProduct.supplier}</td>
+                                            <td>{oneProduct.memo}</td>
+
+                                            <td>
+                                        
+                                                <LinkContainer to={`/product/${oneProduct._id}/edit`}>
+                                                    <Button variant='light' className='btn-sm'>
+                                                        <i className='fas fa-edit'></i>
+                                                    </Button>
+                                                </LinkContainer>
+
+                                                <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(oneProduct._id)}>
+                                                    <i className='fas fa-trash'></i>
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                            <Paginate pages='' page='' isAdmin={true} />
+                        </div>
+                    )}
+        </div>
+    )
+}
+
+export default OrderListScreen
