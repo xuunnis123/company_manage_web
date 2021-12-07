95857e74fa9fcbab0331316317a9adc4da118436
Ji-Yang Lin <xuunnis123@gmail.com>
2021-12-07 23:46:40 +0800

    modified Some Order variables

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,15 +11,23 @@ import {
     ORDER_DETAILS_FAIL,
 
 
-    ORDER_LIST_MY_REQUEST,
-    ORDER_LIST_MY_SUCCESS,
-    ORDER_LIST_MY_FAIL,
-    ORDER_LIST_MY_RESET,
-
-
+    
     ORDER_LIST_REQUEST,
     ORDER_LIST_SUCCESS,
-    ORDER_LIST_FAIL
+    ORDER_LIST_FAIL,
+
+
+    ORDER_DELETE_REQUEST,
+    ORDER_DELETE_SUCCESS,
+    ORDER_DELETE_FAIL,
+
+
+
+
+    ORDER_UPDATE_REQUEST,
+    ORDER_UPDATE_SUCCESS,
+    ORDER_UPDATE_FAIL,
+    ORDER_UPDATE_RESET,
 
 } from '../constants/orderConstants'
 
@@ -52,3 +60,61 @@ export const orderCreateReducer = (state = {}, action) => {
             return state
     }
 }
+
+
+
+export const orderDetailsReducer = (state = { order: { } }, action) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return { loading: true, ...state }
+
+        case ORDER_DETAILS_SUCCESS:
+            return { loading: false, order: action.payload }
+
+        case ORDER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+
+export const orderDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_DELETE_REQUEST:
+            return { loading: true }
+
+        case ORDER_DELETE_SUCCESS:
+            return { loading: false, success: true }
+
+        case ORDER_DELETE_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+
+
+export const orderUpdateReducer = (state = { order: {} }, action) => {
+    switch (action.type) {
+        case ORDER_UPDATE_REQUEST:
+            return { loading: true }
+
+        case ORDER_UPDATE_SUCCESS:
+            return { loading: false, success: true, order: action.payload }
+
+        case ORDER_UPDATE_FAIL:
+            return { loading: false, error: action.payload }
+
+        case ORDER_UPDATE_RESET:
+            return { order: {} }
+
+        default:
+            return state
+    }
+}
+
+
